fd6fa985ad13c7b7f75e6a9e8e937ef9637cc407
Julien Havel <julien.havel@outlook.com>
2022-05-31 11:23:24 +0200

    Added data naming with volumes

--- beerscan/webscraping/bierespeciale.py
+++ beerscan/webscraping/bierespeciale.py
@@ -4,17 +4,24 @@ from urllib.request import urlretrieve
 
 import numpy as np
 import pandas as pd
-
+import re
 
 def scrape_images(url):
     html = requests.get(url)
     soup = BeautifulSoup(html.content, 'html.parser')
 
-    images = soup.find_all("img", {"class": "product-miniature__img"})
-    names = soup.find_all("h2", {"class": "product-miniature__title"})
+    beers = soup.find_all("article")
 
-    names = [name.a.text.lower() for name in names]
-    images = [image["src"] for image in images]
+    images = []
+    names = []
+
+    for beer in beers:
+        image = beer.find("img", {"class": "product-miniature__img"})["src"]
+        beer_name = beer.find("h2", {"class": "product-miniature__title"}).text.strip().lower()
+        volume = beer.find(class_= "product-miniature__feature", string=re.compile("[0-9]cl"))
+        if image and beer_name and volume:
+            names.append(f"{beer_name} - {volume.text.strip()}")
+            images.append(image)
 
     return pd.DataFrame({"beer_name": names,
                          "image_url": images})
@@ -26,10 +33,11 @@ def download_images(scrape_results, path):
     names = scrape_results["beer_name"]
     for image_url, beer_name in zip(images, names):
         extension = "." + image_url.split(".")[-1]
-        image_name = beer_name.replace(" ", "_")\
-                              .replace(".", "")\
-                              .replace("/", "")\
-                              .replace("\\", "") + extension
+        image_name = beer_name.replace(".", " ")\
+                              .replace("/", " ")\
+                              .replace("-", " ")\
+                              .replace("\\", " ") + extension
+        image_name = "_".join(image_name.split())
         try:
             urlretrieve(image_url, path + image_name)
             image_path.append(image_name)
