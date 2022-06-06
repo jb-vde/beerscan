e1accef80ca2bd8ff08a2d335b78d54037fab62b
Julien Havel <julien.havel@outlook.com>
2022-06-06 17:55:41 +0200

    Pipeline implementation - No contrast

---
+++ beerscan/model/pipeline.py
@@ -0,0 +1,116 @@
+"""
+    Input JSON with images and boxes
+    Crop image based on boxes
+    Contrast cropped img
+    SIFT cropped img
+    Indentify by comparing features against dataset
+    return JSON with beer name
+"""
+
+
+"""
+    IN :
+
+    img_dict = {
+        box1: {
+            'startX',
+            'startY',
+            'endX',
+            'endY'
+        },
+        box2: {
+            'startX',
+            'startY',
+            'endX',
+            'endY'
+        },
+        ...
+        boxN: {
+            'startX',
+            'startY',
+            'endX',
+            'endY'
+        }
+    }
+
+"""
+from sys import api_version
+from pandas import DataFrame
+from beerscan.model.beer_identification.sift import load_sift_dataset, do_sift, identify
+import cv2
+
+# For pipe testing
+from beerscan.api.query import test_boxes_endpoint
+import base64
+
+NUM_FEATURES = 300
+
+
+def main_pipe(boxes_dict:dict, image) -> dict:
+
+    for key, box in boxes_dict["boxes"].items():
+        #Crop Based on box
+
+        print(box)
+        (startX, startY, endX, endY) = box["startX"], box["startY"], box["endX"], box["endY"]
+        image_cropped = image[startY:endY, startX:endX, :]
+
+        # Contrast cropped image
+
+
+        # SIFT cropped image
+        keypoints, descriptors = do_sift(image_cropped, NUM_FEATURES)
+
+        # Identify cropped image
+
+        sift_dataset = load_sift_dataset()
+        boxes_dict["boxes"][key]["beer_name"] = identify(descriptors, sift_dataset, number=1)["beer_name"]
+
+    return boxes_dict
+
+"""
+OUT:
+
+    img_dict = {
+        box1: {
+            'startX',
+            'startY',
+            'endX',
+            'endY',
+            'beer_name'
+        },
+        box2: {
+            'startX',
+            'startY',
+            'endX',
+            'endY',
+            'beer_name'
+        },
+        ...
+        boxN: {
+            'startX',
+            'startY',
+            'endX',
+            'endY',
+            'beer_name'
+        }
+    }
+
+"""
+
+if __name__ == "__main__":
+
+    # Get the boxes
+
+    image_file = 'raw_data/images/test_img/belgian_beer_tour.jpg'
+    image = cv2.imread(image_file)
+
+    with open(image_file, "rb") as f:
+        im_bytes = f.read()
+    img_b64 = base64.b64encode(im_bytes).decode("utf8")
+
+    data = test_boxes_endpoint(img_b64)
+
+
+    # Test the pipe
+    print(main_pipe(data, image))
